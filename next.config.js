9c44e215d8c7355b530bc605f9d09e89f8cc4968
Artem Shashkin <temash@gmail.com>
2022-12-18 19:19:12 +0100

    Test

--- next.config.js
+++ next.config.js
@@ -18,6 +18,11 @@ const nextConfig = {
   reactStrictMode: true,
   typescript: {
     tsconfigPath,
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
   },
 }
 
